a5c99b8b0adefb9f0df7353d3f3f129707f958be
rap2h <raphaelht@gmail.com>
2017-05-16 21:48:17 +0200

    hop

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ extern crate rand;
 use rand::Rng;
 use std::fs::File;
 use std::path::Path;
-use image::{ImageBuffer, Rgba, ImageRgba8, PNG, RgbaImage};
+use image::{ImageBuffer, Rgb, ImageRgb8, PNG, RgbImage};
 
 pub struct Mountain {
-    color: Rgba<u8>,
+    color: Rgb<u8>,
     points: Vec<u32>,
-    y_amp: [u8; 2],
+    y_amp: [u32; 2],
 }
 
 impl Mountain {
-    fn new(color: Rgba<u8>, y_amp: [u8; 2]) -> Mountain {
+    fn new(color: Rgb<u8>, y_amp: [u32; 2]) -> Mountain {
         let step_max = 1.75;
         let step_change = 0.75;
         let height_max = (y_amp[1] - 1) as f64;
@@ -24,7 +24,7 @@ impl Mountain {
 
         let mut points: Vec<u32> = Vec::new();
 
-        for _ in 0..320 {
+        for _ in 0..640 {
             height = height + slope;
             slope = slope + (rng.gen_range(0.0, step_change) * 2.0 - step_change);
 
@@ -51,11 +51,11 @@ impl Mountain {
             y_amp: y_amp
         }
     }
-    fn draw(&self, img: &mut RgbaImage) {
+    fn draw(&self, img: &mut RgbImage) {
         let mut i = 0;
         for &point in self.points.iter() {
             img.put_pixel(i, point, self.color);
-            for j in point..200 {
+            for j in point..self.y_amp[1] {
                 img.put_pixel(i, j, self.color);
             }
             i = i + 1;
@@ -64,15 +64,28 @@ impl Mountain {
 }
 
 fn main() {
-    let mut img = ImageBuffer::new(320, 200);
-    
-    let m = Mountain::new(Rgba([255, 0, 0, 255]), [20, 200]);
-    m.draw(&mut img);
-    let m = Mountain::new(Rgba([255, 255, 0, 255]), [100, 200]);
-    m.draw(&mut img);
-    let m = Mountain::new(Rgba([0, 0, 230, 255]), [140, 180]);
-    m.draw(&mut img);
+    let mut img = ImageBuffer::new(640, 480);
+    let mut rng = rand::thread_rng();
+
+    let initial_color: (u8, u8, u8) = (
+       rng.gen_range(1,255),
+       rng.gen_range(1,255), 
+       rng.gen_range(1,255)
+    );
+
+    let mountain_count: u8 = 5;
+
+    for i in 0..mountain_count {
+        
+        let m = Mountain::new(
+            Rgb([
+                initial_color.0 / mountain_count * (i+1) as u8, 
+                initial_color.1  / mountain_count * (i+1) as u8, 
+                initial_color.2  / mountain_count * (i+1) as u8
+            ]), [480 - 480 / 2 / mountain_count as u32 * (mountain_count as u32 - i as u32) , 480]);
+        m.draw(&mut img);
+    }
     
     let ref mut fout = File::create(&Path::new("images/export.png")).unwrap();
-    let _    = ImageRgba8(img).save(fout, PNG);
+    let _    = ImageRgb8(img).save(fout, PNG);
 }
