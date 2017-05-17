73edee81be75851901a91f801126927a98116063
rap2h <raphaelht@gmail.com>
2017-05-17 15:26:35 +0200

    hop

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ use image::{ImageBuffer, Rgb, ImageRgb8, PNG, RgbImage};
 
 pub struct Mountain {
     color: Rgb<u8>,
-    points: Vec<u32>,
-    y_amp: [u32; 2],
+    points: Vec<u32>
 }
 
 impl Mountain {
     fn new(color: Rgb<u8>, y_amp: [u32; 2]) -> Mountain {
-        let step_max = 1.75;
-        let step_change = 0.75;
+        let step_max = 1.0;
+        let step_change = 0.25;
         let height_max = (y_amp[1] - 1) as f64;
         let height_min = (y_amp[0]) as f64;
         let mut rng = rand::thread_rng();
@@ -47,15 +46,14 @@ impl Mountain {
         }
         Mountain {
             color: color,
-            points: points,
-            y_amp: y_amp,
+            points: points
         }
     }
     fn draw(&self, img: &mut RgbImage) {
         let mut i = 0;
         for &point in self.points.iter() {
             img.put_pixel(i, point, self.color);
-            for j in point..self.y_amp[1] {
+            for j in point..480 {
                 img.put_pixel(i, j, self.color);
             }
             i = i + 1;
@@ -63,27 +61,36 @@ impl Mountain {
     }
 }
 
+fn color_step(c1: u8, c2: u8, total: u8, step: u8) -> u8 {
+   (c1 as i16 + (c2 as i16 - c1 as i16) / total as i16  * step as i16) as u8
+}
+
 fn main() {
-    let mut img = ImageBuffer::new(640, 480);
     let mut rng = rand::thread_rng();
 
-    let initial_color: (u8, u8, u8) =
-        (rng.gen_range(1, 255), rng.gen_range(1, 255), rng.gen_range(1, 255));
+    let initial_color = (rng.gen_range(1, 255), rng.gen_range(1, 255), rng.gen_range(1, 255));
+    let target_color = match rng.gen_range(1, 4) {
+        1 => (rng.gen_range(0,30),rng.gen_range(0,30),rng.gen_range(0,30)),
+        2 => (220, 220, rng.gen_range(230, 255)),
+        _ => (rng.gen_range(200, 255),rng.gen_range(200, 255),rng.gen_range(200, 255)),
+    };
 
     let mountain_count: u8 = 5;
+
+    let mut img = ImageBuffer::from_pixel(640, 480, Rgb([target_color.0, target_color.1, target_color.2]));
 
     for i in 0..mountain_count {
         let m = Mountain::new(
             Rgb(
                 [
-                    initial_color.0 / mountain_count * (i + 1) as u8,
-                    initial_color.1 / mountain_count * (i + 1) as u8,
-                    initial_color.2 / mountain_count * (i + 1) as u8,
+                    color_step(target_color.0, initial_color.0, mountain_count, (i + 1) as u8),
+                    color_step(target_color.1, initial_color.1, mountain_count, (i + 1) as u8),
+                    color_step(target_color.2, initial_color.2, mountain_count, (i + 1) as u8),
                 ]
             ),
             [
                 400 - 480 / 2 / mountain_count as u32 * (mountain_count as u32 - i as u32),
-                480,
+                401,
             ],
         );
         m.draw(&mut img);
