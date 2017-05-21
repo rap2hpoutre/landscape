40ce837eb7dfc7d54b5b7cfeafc30b2a557593d6
rap2h <raphaelht@gmail.com>
2017-05-21 21:40:47 +0200

    hop

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ extern crate image;
 extern crate imageproc;
 extern crate rand;
 
-use rand::Rng;
+use rand::{Rng, ThreadRng};
 use std::fs::File;
 use std::path::Path;
 use image::{ImageBuffer, Rgb, ImageRgb8, PNG, RgbImage};
 use imageproc::drawing::draw_filled_circle_mut;
+use imageproc::pixelops::interpolate;
 
 struct Mountain {
     color: Rgb<u8>,
@@ -15,14 +16,13 @@ struct Mountain {
 
 impl Mountain {
     fn new(color: Rgb<u8>, y_amp: [u32; 2]) -> Mountain {
-        let step_max = 1.0;
-        let step_change = 0.25;
+        let mut rng = rand::thread_rng();
+        let step_max = rng.gen_range(0.9, 1.1);
+        let step_change = rng.gen_range(0.15, 0.35);
         let height_max = (y_amp[1] - 1) as f64;
         let height_min = (y_amp[0]) as f64;
-        let mut rng = rand::thread_rng();
         let mut height = rng.gen_range(0.0, height_max);
         let mut slope = rng.gen_range(0.0, step_max) * 2.0 - step_max;
-
         let mut points: Vec<u32> = Vec::new();
 
         for _ in 0..640 {
@@ -35,7 +35,6 @@ impl Mountain {
             if slope < -step_max {
                 slope = -step_max;
             }
-
             if height > height_max {
                 height = height_max;
                 slope = slope * -1.0;
@@ -63,56 +62,36 @@ impl Mountain {
     }
 }
 
-struct Planet {
-    color: Rgb<u8>,
-    position: (i32, i32),
-    radius: i32
-}
-
-impl Planet {
-    fn new(color: Rgb<u8>, position: (i32, i32), radius: i32) -> Planet {
-        Planet {
-            color: color,
-            position: position,
-            radius: radius,
-        }
-    }
-    fn draw(&self, img: &mut RgbImage) {
-        draw_filled_circle_mut(img, (self.position.0, self.position.1), self.radius, self.color);
-    }
-}
-
-fn color_mix(c1: (u8, u8, u8), c2: (u8, u8, u8), total: u8, step: u8) ->  Rgb<u8> {
-    Rgb([
-        (c1.0 as i16 + (c2.0 as i16 - c1.0 as i16) / total as i16  * step as i16) as u8,
-        (c1.1 as i16 + (c2.1 as i16 - c1.1 as i16) / total as i16  * step as i16) as u8,
-        (c1.2 as i16 + (c2.2 as i16 - c1.2 as i16) / total as i16  * step as i16) as u8,
-    ])
+fn rgb_rand(rng: &mut ThreadRng, r: (u8, u8), g: (u8, u8), b: (u8, u8)) -> Rgb<u8> {
+    Rgb([rng.gen_range(r.0, r.1), rng.gen_range(g.0, g.1), rng.gen_range(b.0, b.1)])
 }
 
 fn main() {
     let mut rng = rand::thread_rng();
-
-    let initial_color = (rng.gen_range(1, 255), rng.gen_range(1, 255), rng.gen_range(1, 255));
-    let target_color = match rng.gen_range(1, 4) {
-        1 => (rng.gen_range(0,30),rng.gen_range(0,30),rng.gen_range(0,30)),
-        2 => (220, 220, rng.gen_range(230, 255)),
-        _ => (rng.gen_range(200, 255),rng.gen_range(200, 255),rng.gen_range(200, 255)),
+    let c_sky = match rng.gen_range(1, 4) {
+        1 => rgb_rand(&mut rng, (1, 40), (1, 40), (1, 40)),
+        2 => rgb_rand(&mut rng, (215, 225), (215, 225), (230, 255)),
+        _ => rgb_rand(&mut rng, (200, 255), (200, 255), (200, 255)),
     };
-    let planet_color = (rng.gen_range(1, 255), rng.gen_range(1, 255), rng.gen_range(1, 255));
+    let mut img = ImageBuffer::from_pixel(640, 480, c_sky);
 
-    let mountain_count: u8 = 5;
-
-    let mut img = ImageBuffer::from_pixel(640, 480, Rgb([target_color.0, target_color.1, target_color.2]));
-
-    Planet::new(color_mix(target_color, planet_color, 8, 1), (rng.gen_range(101, 500), rng.gen_range(101, 200)), rng.gen_range(40, 200)).draw(&mut img);
-
+    if rng.gen_weighted_bool(2) {
+        let x = rng.gen_range(101, 520);
+        let y = rng.gen_range(81, 200);
+        let rad = rng.gen_range(20, 80);
+        let c_planet = interpolate(rgb_rand(&mut rng, (1, 255), (1, 255), (1, 255)), c_sky, 0.1);
+        draw_filled_circle_mut(&mut img, (x, y), rad, c_planet);
+        if rng.gen_weighted_bool(2) {
+            draw_filled_circle_mut(&mut img, (x + rng.gen_range(-2, 4) * 10, y), rad, c_sky);
+        }
+    }
+    
+    let mountain_count: u32 = rng.gen_range(4, 7);
+    let c_mountain = rgb_rand(&mut rng, (1, 255), (1, 255), (1, 255));
     for i in 0..mountain_count {
-        let m = Mountain::new(
-            color_mix(target_color, initial_color, mountain_count, (i + 1) as u8),
-            [ 400 - 480 / 2 / mountain_count as u32 * (mountain_count as u32 - i as u32), 401 ],
-        );
-        m.draw(&mut img);
+        let c = interpolate(c_mountain, c_sky, (i + 1) as f32 / mountain_count as f32);
+        let y_amp = [ 400 - 480 / 2 / mountain_count * (mountain_count - i), 401 ];
+        Mountain::new(c, y_amp).draw(&mut img);
     }
 
     let ref mut fout = File::create(&Path::new("images/export.png")).unwrap();
